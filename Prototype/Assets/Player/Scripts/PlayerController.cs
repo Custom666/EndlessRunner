f33caa8cd12a460ae27de3bc81a18c7dcfa1f093
custom666 <32340961+custom666@users.noreply.github.com>
2018-03-24 17:51:09 +0100

    added better planet

---
+++ Prototype/Assets/Player/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace Assets.Player.Scripts
+{
+    public class PlayerController : MonoBehaviour
+    {
+        public float Jump = 5f;
+
+        private Rigidbody _rigidbody;
+        private Animator _animator;
+
+        private float _distToGround;
+
+        // Use this for initialization
+        private void Start()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+            _animator = GetComponent<Animator>();
+
+            _distToGround = GetComponent<Collider>().bounds.extents.y;
+        }
+
+        private void FixedUpdate()
+        {
+            if (Input.GetButtonDown("Jump") && IsGrounded())
+                _rigidbody.AddForce(Vector3.up * Jump, ForceMode.Impulse);
+        }
+
+        public bool IsGrounded()
+        {
+            return Physics.Raycast(transform.position, -Vector3.up, _distToGround);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            
+        }
+    }
+}
