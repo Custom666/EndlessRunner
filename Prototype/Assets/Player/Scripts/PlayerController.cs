ac0d56ee6d32086d56b2e6c7c89edcee8c029859
custom666 <32340961+custom666@users.noreply.github.com>
2018-03-25 16:32:19 +0200

    Change player and environment to 2D

--- Prototype/Assets/Player/Scripts/PlayerController.cs
+++ Prototype/Assets/Player/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Assets.Player.Scripts
 {
@@ -6,34 +7,33 @@ namespace Assets.Player.Scripts
     {
         public float Jump = 5f;
 
-        private Rigidbody _rigidbody;
+        private Rigidbody2D _rigidbody;
         private Animator _animator;
 
         private float _distToGround;
+        private bool _isGrounded = true;
 
         // Use this for initialization
         private void Start()
         {
-            _rigidbody = GetComponent<Rigidbody>();
+            _rigidbody = GetComponent<Rigidbody2D>();
             _animator = GetComponent<Animator>();
 
-            _distToGround = GetComponent<Collider>().bounds.extents.y;
+            _distToGround = GetComponent<Collider2D>().bounds.extents.y;
         }
 
         private void FixedUpdate()
         {
-            if (Input.GetButtonDown("Jump") && IsGrounded())
-                _rigidbody.AddForce(Vector3.up * Jump, ForceMode.Impulse);
+            if (Input.GetButtonDown("Jump") && _isGrounded)
+            {
+                _rigidbody.AddForce(Vector2.up * Jump, ForceMode2D.Impulse);
+                _isGrounded = false;
+            }
         }
-
-        public bool IsGrounded()
+        
+        private void OnCollisionEnter2D(Collision2D other)
         {
-            return Physics.Raycast(transform.position, -Vector3.up, _distToGround);
-        }
-
-        private void OnTriggerEnter(Collider other)
-        {
-            
+            if (string.CompareOrdinal("Planet", other.gameObject.name) == 0) _isGrounded = true;
         }
     }
 }
