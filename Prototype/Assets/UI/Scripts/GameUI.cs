66e28dc251dcc0cb6a9af62329d47c8444335ded
custom666 <32340961+custom666@users.noreply.github.com>
2018-04-01 19:39:13 +0200

    Added main menu scene

---
+++ Prototype/Assets/UI/Scripts/GameUI.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Assets.Enemies.Scripts;
+using Assets.Player.Scripts;
+using Assets.Projectiles.Scripts;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Assets.UI.Scripts
+{
+    public class GameUI : MonoBehaviour
+    {
+        private Text _playerHealth;
+
+        private Slider _enemyHealth;
+
+        private GameObject _gameMenu;
+
+        private void Start()
+        {
+            _playerHealth = transform.GetChild(0).transform.GetChild(0).gameObject.GetComponent<Text>();
+            _enemyHealth = transform.GetChild(1).transform.GetChild(0).gameObject.GetComponent<Slider>();
+            _gameMenu = transform.GetChild(2).gameObject;
+
+            _enemyHealth.onValueChanged.AddListener((value) =>
+            {
+                transform.GetChild(1).transform.GetChild(1).gameObject.GetComponent<Text>().text = "Enemy: " + value;
+            });
+        }
+
+        private void LateUpdate()
+        {
+            if(Input.GetButtonDown("Pause")) _gameMenu.SetActive(true);
+        }
+
+        private void OnEnable()
+        {
+            PlayerController.OnHealthChangedEvent += PlayerOnHealthChangedEvent;
+            ProjectileController.OnEnemyHitEvent += ProjectileOnEnemyHitEvent;
+        }
+
+        private void OnDisable()
+        {
+            PlayerController.OnHealthChangedEvent -= PlayerOnHealthChangedEvent;
+            ProjectileController.OnEnemyHitEvent -= ProjectileOnEnemyHitEvent;
+        }
+
+        private void PlayerOnHealthChangedEvent(int health)
+        {
+            _playerHealth.text = "Healths: " + health;
+
+            if (health <= 0)
+            {
+                Time.timeScale = 0f;
+
+                _gameMenu.transform.Find("GameMenuText").GetComponent<Text>().text = "GAME OVER";
+
+                _gameMenu.SetActive(true);
+            }
+        }
+
+        private void ProjectileOnEnemyHitEvent()
+        {
+            _enemyHealth.value -= 1;
+
+            if (_enemyHealth.value <= 0)
+            {
+                Time.timeScale = 0f;
+
+                _gameMenu.transform.Find("GameMenuText").GetComponent<Text>().text = "VICTORY";
+
+                _gameMenu.SetActive(true);
+            }
+        }
+    }
+}
---
+++ Prototype/Assets/UI/Scripts/MenuUI.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Assets.Enemies.Scripts;
+using Assets.Player.Scripts;
+using Assets.Projectiles.Scripts;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Assets.UI.Scripts
+{
+    public class MenuUI : MonoBehaviour
+    {
+        private void LateUpdate()
+        {
+            if (Input.GetButtonDown("Pause")) Pause(gameObject);
+        }
+
+        public void Quit()
+        {
+            Application.Quit();    
+        }
+
+        public void Restart()
+        {
+            SceneManager.LoadSceneAsync(1, LoadSceneMode.Single);
+
+            Time.timeScale = 1f;
+        }
+
+        public void MainMenu()
+        {
+            SceneManager.LoadSceneAsync(0, LoadSceneMode.Single);
+        }
+
+        public void Pause(GameObject menu)
+        {
+            menu.SetActive(true);
+
+            Time.timeScale = 0f;
+        }
+
+        public void Resume(GameObject menu)
+        {
+            menu.SetActive(false);
+
+            Time.timeScale = 1f;
+        }
+    }
+}
