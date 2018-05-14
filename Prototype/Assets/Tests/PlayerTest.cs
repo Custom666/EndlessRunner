64a951d7ffe9573e5e69270083e4add9d40a7ac9
Custom666 <vitek.poor@seznam.cz>
2018-05-14 16:23:43 +0200

    Added initial player test

---
+++ Prototype/Assets/Tests/PlayerTest.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using Assets.Player.Scripts;
+
+public class PlayerTest {
+
+    private GameObject _terrain;
+    private GameObject _player;
+
+    [SetUp]
+    public void Setup()
+    {
+        _terrain = Object.Instantiate(Resources.Load<GameObject>("Land"));
+        _player = Object.Instantiate(Resources.Load<GameObject>("Player"));
+    }
+
+    [UnityTest]
+    public IEnumerator CollisonWithCraterTest()
+    {
+        var crater = Object.Instantiate(Resources.Load<GameObject>("Crater"));
+
+        yield return new WaitUntil(() => Physics.CheckBox(_player.transform.position, crater.transform.position));
+
+        Assert.True(_player.GetComponent<PlayerController>().Health == 0);
+    }
+}
