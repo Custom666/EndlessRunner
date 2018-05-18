d24868aa017e7f53e45885c37dc32bab4fbeeeb7
custom666 <32340961+custom666@users.noreply.github.com>
2018-05-18 21:47:02 +0200

    Added player UI health

--- Prototype/Assets/UI/Scripts/GameUI.cs
+++ Prototype/Assets/UI/Scripts/GameUI.cs
@@ -13,15 +13,12 @@ namespace Assets.UI.Scripts
     /// </summary>
     public class GameUI : MonoBehaviour
     {
-        private Text _playerHealth;
-
         private Slider _enemyHealth;
 
         private GameObject _gameMenu;
         
         private void Awake()
         {
-            _playerHealth = transform.GetChild(0).transform.GetChild(0).gameObject.GetComponent<Text>();
             _enemyHealth = transform.GetChild(1).transform.GetChild(0).gameObject.GetComponent<Slider>();
             _gameMenu = transform.GetChild(2).gameObject;
 
@@ -44,37 +41,14 @@ namespace Assets.UI.Scripts
 
         private void OnEnable()
         {
-            PlayerController.OnHealthChangedEvent += PlayerOnHealthChangedEvent;
             ProjectileController.OnEnemyHitEvent += ProjectileOnEnemyHitEvent;
         }
 
         private void OnDisable()
         {
-            PlayerController.OnHealthChangedEvent -= PlayerOnHealthChangedEvent;
             ProjectileController.OnEnemyHitEvent -= ProjectileOnEnemyHitEvent;
         }
-
-        private void PlayerOnHealthChangedEvent(int health)
-        {
-            _playerHealth.text = "Healths: " + health;
-
-            #if UNITY_EDITOR
-                return;
-            #endif
-            
-            if (health <= 0)
-            {
-                Time.timeScale = 0f;
-
-                _gameMenu.transform.Find("GameMenuText").GetComponent<Text>().text = "GAME OVER";
-
-                // deactivate resume button
-                _gameMenu.transform.GetChild(1).gameObject.SetActive(false);
-
-                _gameMenu.SetActive(true);
-            }
-        }
-
+        
         private void ProjectileOnEnemyHitEvent()
         {
             _enemyHealth.value -= 1;
---
+++ Prototype/Assets/UI/Scripts/PlayerUI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets.Player.Scripts;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerUI : MonoBehaviour
+{
+    [SerializeField]
+    private Slider Oxygen;
+    
+    private void Update()
+    {
+        Oxygen.value -= Time.deltaTime;
+    }
+
+    private void OnEnable()
+    {
+        PlayerController.OnHealthChangedEvent += PlayerOnHealthChangedEvent;
+    }
+
+    private void OnDisable()
+    {
+        PlayerController.OnHealthChangedEvent -= PlayerOnHealthChangedEvent;
+    }
+
+    private void PlayerOnHealthChangedEvent(int health)
+    {
+
+#if UNITY_EDITOR
+        return;
+#endif
+    }
+}
